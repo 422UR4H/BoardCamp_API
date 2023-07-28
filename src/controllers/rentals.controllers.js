d9a83fd7fa20df7a4f061e8b686bb838fdc0fd0c
422UR4H <tiagosindra@id.uff.br>
2023-07-28 02:56:20 -0300

    feat/createRental

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -1,15 +1,69 @@
-export function createRental(req, res) {
+import { db } from "../database/database.js";
+import dayjs from "dayjs";
 
+
+export async function createRental(req, res) {
+    const { customerId, gameId, daysRented } = res.locals.body;
+    if (!customerId || !gameId || !daysRented) return console.log(res.locals.body)
+
+    try {
+        const customer = (await db.query(
+            `SELECT name FROM customers
+            WHERE id = $1`,
+            [customerId]
+        )).rows[0];
+        if (customer === undefined) return res.status(400).send("Este Cliente não existe!");
+
+        const { pricePerDay, stockTotal } = (await db.query(
+            `SELECT pricePerDay, stockTotal FROM games
+            WHERE id = $1`,
+            [gameId]
+        )).rows[0];
+        if (pricePerDay === undefined) return res.status(400).send("Este jogo não existe!");
+
+        const rentals = (await db.query(
+            `SELECT id FROM rentals
+            WHERE gameId = $1`,
+            [gameId]
+        )).rows;
+        if (rows.length >= stockTotal) return res.status(400).send("Este jogo está indisponível!");
+
+        const originalPrice = pricePerDay * daysRented;
+        const rentDate = dayjs().locale("pt-br").format("YYYY-MM-DD");
+        const { rowCount } = await db.query(
+            `INSERT INTO rentals
+            (customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+            [customerId, gameId, rentDate, daysRented, null, originalPrice, null]
+        );
+        if (rowCount <= 0) return res.sendStatus(500);
+
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 }
 
-export function getAllRentals(req, res) {
+export async function getAllRentals(req, res) {
+    try {
 
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 }
 
-export function finishRental(req, res) {
+export async function finishRental(req, res) {
+    try {
 
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 }
 
-export function deleteRental(req, res) {
-    
+export async function deleteRental(req, res) {
+    try {
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 }
