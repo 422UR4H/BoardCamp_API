1a3e5bcb294f6cf5839ebe8e080cceb49c3d3d28
422UR4H <tiagosindra@id.uff.br>
2023-07-27 22:29:29 -0300

    chore

--- src/database/database.js
+++ src/database/database.js
@@ -1,9 +1,14 @@
+import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const connection = new Pool({
+const { Pool } = pg;
+const configDatabase = {
     connectionString: process.env.DATABASE_URL,
-});
+};
 
-export default connection;
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "prod") {
+    configDatabase.ssl = true;
+}
+export const db = new Pool(configDatabase);
