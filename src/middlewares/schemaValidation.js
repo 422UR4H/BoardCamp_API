97af0837157006f5272ed74181c149bb9bc945fd
422UR4H <tiagosindra@id.uff.br>
2023-07-29 16:15:10 -0300

    :construction: testing on deploy 4

--- src/middlewares/schemaValidation.js
+++ src/middlewares/schemaValidation.js
@@ -1,7 +1,7 @@
 export default function schemaValidation(schema) {
     return (req, res, next) => {
         const { error, value } = schema.validate(req.body, { abortEarly: false });
-        if (error) return res.status(400).send(error.details.map(d => d.message));
+        //if (error) return res.status(400).send(error.details.map(d => d.message));
         res.locals.body = value;
         next();
     };
