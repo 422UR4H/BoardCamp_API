5224b1354129bb3368e306c1cc4f206312bb86b6
422UR4H <tiagosindra@id.uff.br>
2023-07-29 14:09:24 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express, { json } from "express";
 import cors from "cors";
 import router from "./routes/index.routes.js";
+import dotenv from "dotenv";
 
 
 const app = express();
 app.use(json());
 app.use(cors());
 app.use(router);
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- src/schemas/games.schemas.js
+++ src/schemas/games.schemas.js
@@ -3,7 +3,7 @@ import { stripHtml } from "string-strip-html";
 
 export const gamesSchema = Joi.object({
     name: Joi.string().custom(value => stripHtml(value)).trim().required(), // .min(3).max(30)
-    image: Joi.string().custom(value => stripHtml(value)).uri(), // required?
+    image: Joi.string().custom(value => stripHtml(value)).uri().required(),
     stockTotal: Joi.number().integer().greater(0).required(),
     pricePerDay: Joi.number().integer().greater(0).required()
 });
