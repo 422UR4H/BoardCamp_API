ffbb62a442f968e38ee92dab7e3de27a9954f577
422UR4H <tiagosindra@id.uff.br>
2023-07-29 15:32:09 -0300

    fix/customersSchema

--- src/schemas/customers.schemas.js
+++ src/schemas/customers.schemas.js
@@ -3,7 +3,7 @@ import { stripHtml } from "string-strip-html";
 
 export const customersSchema = Joi.object({
     name: Joi.string().custom(value => stripHtml(value)).trim().min(3).max(30).required(),
-    phone: Joi.string().custom(value => stripHtml(value)).trim().number().min(10).max(11).required(),
-    cpf: Joi.string().custom(value => stripHtml(value)).trim().number().length(11).required(),
-    birthday: Joi.date().format("YYYY-MM-DD").required()
+    phone: Joi.string().custom(value => stripHtml(value)).trim().pattern(/^[0-9]+$/).min(10).max(11).required(),
+    cpf: Joi.string().custom(value => stripHtml(value)).trim().pattern(/^[0-9]+$/).length(11).required(),
+    birthday: Joi.date().iso().max('now').required()
 });
