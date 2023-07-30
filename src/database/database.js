35a5df3410cc3fb8cacd9ec03c512bd455afa3b1
422UR4H <tiagosindra@id.uff.br>
2023-07-30 13:00:10 -0300

    :construction: testing on deploy 8

--- src/database/database.js
+++ src/database/database.js
@@ -8,7 +8,7 @@ const configDatabase = {
     connectionString: process.env.DATABASE_URL,
 };
 
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "prod") {
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "prod" || process.env.MODE === "production" || process.env.MODE === "prod") {
     configDatabase.ssl = true;
 }
 export const db = new Pool(configDatabase);
